2141ed9940fea1f7d70addc1949d887e1d15afbb
Aubai9 <aubaimahmoud@gmail.com>
2026-06-14 15:35:22 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
@@ -7,35 +6,28 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate", // تحديث التطبيق تلقائياً عند الزبون لو نزلنا تحديث جديد
-      devOptions: {
-        enabled: false, // تفعيل الميزة حتى أثناء البرمجة (Localhost) للتجربة
-      },
+      // 🌟 التعديل السحري: إخبار Vite بدمج السيرفيس ووركر الخاص بنا في نسخة الرفع 🌟
+      strategies: "injectManifest",
+      srcDir: "public",
+      filename: "custom-sw.js",
+
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
       manifest: {
-        name: "متجر خضر الإلكتروني", // الاسم الكامل للتطبيق
-        short_name: "متجر خضر", // الاسم القصير الذي سيظهر تحت الأيقونة بشاشة الموبايل
-        description: "أفضل تجربة تسوق إلكتروني بأسرع وأسهل طريقة",
-        theme_color: "#001f3f", // لون شريط الموبايل العلوي (كحلي فخم)
-        background_color: "#ffffff", // لون شاشة التحميل (Splash Screen)
-        display: "standalone", // هذه الكلمة هي السحر! تجعل التطبيق يفتح بدون شريط المتصفح كأنه تطبيق أصلي
-        orientation: "portrait", // تثبيت التطبيق بالطول
+        name: "متجر PWA",
+        short_name: "المتجر",
+        theme_color: "#001f3f",
+        background_color: "#ffffff",
+        display: "standalone",
         icons: [
-          {
-            src: "/icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
+          { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
           {
             src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable", // ليتوافق مع جميع أشكال أيقونات الأندرويد والآيفون
+            purpose: "any maskable",
           },
         ],
-      },
-      workbox: {
-        cleanupOutdatedCaches: true,
-        importScripts: ["/custom-sw.js"],
       },
     }),
   ],
